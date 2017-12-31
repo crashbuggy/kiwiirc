19e5d869da56c1f476e8cebac5fc28bd1f3196ff
Michael Chesterton <michael.chesterton@gmail.com>
2018-01-01 07:42:42 +1100

    add dir and Dockerfile for upstream released versions

---
+++ release/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# WORKDIR will only get set to /kiwiirc if WORKDIR isn't already set
+: ${WORKDIR:=/kiwiirc}
+
+if [ -f ${WORKDIR}-data/config.conf ]; then
+	ln -sf ${WORKDIR}-data/config.conf
+elif [ -d ${WORKDIR}-data/ ]; then
+	cp config.conf.example ${WORKDIR}-data/config.conf
+	ln -sf ${WORKDIR}-data/config.conf
+else
+	cd $WORKDIR
+	cp config.conf.sample config.conf
+fi
+
+if [ -f ${WORKDIR}-data/config.json ]; then
+	(cd www/static; ln -sf ${WORKDIR}-data/config.json)
+elif [ -d ${WORKDIR}-data ]; then
+	cp www/static/config.json ${WORKDIR}-data
+	(cd www/static; ln -sf ${WORKDIR}-data/config.json)
+fi
+
+exec "$@"
